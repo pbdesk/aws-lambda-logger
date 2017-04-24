d5194bb6dff754124b2498c4ad0857da6a2960c7
Pinal Bhatt <pinal.bhatt@weightwatchers.com>
2017-04-24 13:58:49 -0400

    0.0.3

--- examples/simple.js
+++ examples/simple.js
@@ -1,25 +1,12 @@
 const logger = require('./../index');
 
-const Log = logger.set('INFO', {appName: 'Demo'}, ['Tag1','Tag2']);
+let Log = logger.set('INFO', {appName: 'Demo'}, ['Tag1','Tag2']);
 
 Log.info('Simple message', {info: 'testing'}, ['Tag3']);
-
-/* Output:
- {
-  "msg": "Simple message",
-  "appName": "Demo",
-  "info": "testing",
-  "_tags": [
-    "INFO",
-    "Tag1",
-    "Tag2",
-    "Tag3"
-    ]
- }
-
- */
-
-Log.debug('Some other message', 'meta as string', ['Tag4']);
+Log.warn('Simple message', {warn: 'testing'}, ['Tag3']);
+Log.error('Simple message', {error: 'testing'}, ['Tag3']);
 
 
-Log.info('Some other message', ['ddd'], ['Tag4']);
+let sLog = logger.set('DEBUG');
+
+sLog.sinfo('something simple');
